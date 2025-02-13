1d4f2639e1d2786d2140a3ba8b91d6efa7cb4d42
Jeff Xie <37353357+lumosityfan@users.noreply.github.com>
2025-02-12 20:26:52 -0600

    Added AnchorEntity to Arena file
    Implemented ability to add anchor points to common space

--- masters_project/View/Chess/Arena.swift
+++ masters_project/View/Chess/Arena.swift
@@ -2,13 +2,16 @@ import SwiftUI
 import RealityKit
 import RealityKitContent
 
+let anchor = AnchorEntity(.plane(.horizontal, classification: .any, minimumBounds: SIMD2<Float>(0.2, 0.2)))
 struct Arena: View {
     @EnvironmentObject var model: AppModel
     @Environment(\.physicalMetrics) var physicalMetrics
     var body: some View {
         RealityView { content, attachments in
+            content.add(anchor)
             if let immersiveContentEntity = try? await Entity(named: "Immersive", in: realityKitContentBundle) {
-                content.add(immersiveContentEntity)
+                anchor.addChild(model)
+                model.move(to: Transform(scale: SIMD3(x: 1, y: 1, z: 1), translation: SIMD3<Float>(0.01, 0.01, 0.01)), relativeTo: anchor)
             }
         } attachments: {
             Attachment(id: "board") {
