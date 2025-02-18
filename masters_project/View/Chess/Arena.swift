db294cf54cee6b8dfceb23814be1f3c507bc164a
Jeff Xie <37353357+lumosityfan@users.noreply.github.com>
2025-02-18 15:56:09 -0600

    Added ability to not have road move with person

--- masters_project/View/Chess/Arena.swift
+++ masters_project/View/Chess/Arena.swift
@@ -2,16 +2,22 @@ import SwiftUI
 import RealityKit
 import RealityKitContent
 
+//let tableTransform = Transform(translation: SIMD3<Float>(x: 0, y: -0.62, z: -1))
 let anchor = AnchorEntity(.plane(.horizontal, classification: .any, minimumBounds: SIMD2<Float>(0.2, 0.2)))
 struct Arena: View {
     @EnvironmentObject var model: AppModel
     @Environment(\.physicalMetrics) var physicalMetrics
     var body: some View {
         RealityView { content, attachments in
-            content.add(anchor)
+            //content.add(anchor)
             if let immersiveContentEntity = try? await Entity(named: "Immersive", in: realityKitContentBundle) {
                 anchor.addChild(immersiveContentEntity)
-                immersiveContentEntity.move(to: Transform(scale: SIMD3(x: 1, y: 1, z: 1), translation: SIMD3<Float>(0.01, 0.01, 0.01)), relativeTo: anchor)
+                content.add(ImmersiveContentEntity)
+
+                //anchor.addChild(immersiveContentEntity)
+                immersiveContentEntity.move(to: Transform(scale: SIMD3(x: 0.1, y: 0.1, z: 0.1), translation: SIMD3<Float>(0.5, -1, -1)), relativeTo: anchor)
+                //anchor.addChild(immersiveContentEntity)
+                //content.add(anchor)
             }
         } attachments: {
             Attachment(id: "board") {
