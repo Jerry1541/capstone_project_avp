53e7d54163db6c5b665f493e83b7df856b283da6
Jerrin Thomas <jerrinthomas1541@gmail.com>
2025-04-03 20:02:43 -0500

    [Major update] [Files] - Arena file, RCP content, Assets [Changes] - Scene locking, updated scene content (models, animations), image detection

--- masters_project/View/Chess/Arena.swift
+++ masters_project/View/Chess/Arena.swift
@@ -1,19 +1,88 @@
 import SwiftUI
 import RealityKit
 import RealityKitContent
+import ARKit
 
-let anchor = AnchorEntity(.plane(.horizontal, classification: .any, minimumBounds: SIMD2<Float>(0.2, 0.2)))
 struct Arena: View {
     @EnvironmentObject var model: AppModel
+    //    @Environment(CommonSpaceViewModel.self) var commonModel
     @Environment(\.physicalMetrics) var physicalMetrics
+    //    var commonModel = CommonSpaceViewModel()
+    //    var roadModel = RoadViewModel()
+    //    var arkitViewModel = ARKitViewModel()
+    
+    /*-----------------------------------------------------------------------------------------*/
+    /*Jeff Code for collision*/
+    @State private var collisionBegan: EventSubscription?
+    @State private var collisionEnded: EventSubscription?
+    @Environment(\.realityKitScene) var scene
+    let rknt = "RealityKit.NotificationTrigger"
+    fileprivate func notify(_ scene: RealityKit.Scene) {
+        let notification = Notification(name: .init(rknt), userInfo: ["\(rknt).Scene" : scene, "\(rknt).Identifier" : "notifier"])
+        NotificationCenter.default.post(notification)
+    }
+    /*-----------------------------------------------------------------------------------------*/
+    
+    /*-----------------------------------------------------------------------------------------*/
+    /*Code for image tracking*/
+    let arSession = ARKitSession()
+    let imageInfo = ImageTrackingProvider(referenceImages: ReferenceImage.loadReferenceImages(inGroupNamed: "CardDeck20"))
+    var entity = Entity()
+//    var entityMap : [UUID: ModelEntity] = [:]
+    var rootEntity = Entity()
+    let simpleMaterial = SimpleMaterial(
+        color: .red, isMetallic: false
+    )
+    let anchoring = AnchoringComponent(.image(group: "CardDeck20", name: "IMG_4108"))
+    /*-----------------------------------------------------------------------------------------*/
+    
     var body: some View {
         RealityView { content, attachments in
-            content.add(anchor)
+            
+            //Jeff Code
+            let configuration = SpatialTrackingSession.Configuration(tracking: [.hand])
+            let session = SpatialTrackingSession()
+            await session.run(configuration)
+            
             if let immersiveContentEntity = try? await Entity(named: "Immersive", in: realityKitContentBundle) {
-                anchor.addChild(immersiveContentEntity)
-                immersiveContentEntity.move(to: Transform(scale: SIMD3(x: 1, y: 1, z: 1), translation: SIMD3<Float>(0.01, 0.01, 0.01)), relativeTo: anchor)
+                content.add(immersiveContentEntity)
+                let headAnchor = AnchorEntity(.hand(.right, location: .indexFingerTip))
+                
+                headAnchor.anchoring.physicsSimulation = .none
+                headAnchor.components.set(InputTargetComponent())
+                headAnchor.components.set(ModelComponent(mesh: .generateSphere(radius: 0.01), materials: [SimpleMaterial()]))
+                headAnchor.components.set(CollisionComponent(shapes: [.generateSphere(radius: 0.1)], mode: CollisionComponent.Mode.trigger))
+                content.add(headAnchor)
+                collisionBegan = content.subscribe(to: CollisionEvents.Began.self, on: headAnchor) { collisionEvent in
+                    print("Collision began.")
+                    if let scene {
+                        notify(scene)
+                    }
+                }
+                collisionEnded = content.subscribe(to: CollisionEvents.Ended.self, on: headAnchor) { collisionEvent in
+                    print("Collision ended.")
+                }
+                
             }
-        } attachments: {
+            
+            let _ = await arSession.requestAuthorization(for: [.worldSensing, .handTracking, .cameraAccess])
+            
+            /*if let immersiveContentEntity = try? await Entity(named: "Immersive", in: realityKitContentBundle) {
+                print(immersiveContentEntity)
+                content.add(immersiveContentEntity)
+                
+                immersiveContentEntity.findEntity(named: "Transform")?.anchor?.anchoring = anchoring
+//                immersiveContentEntity.findEntity(named: "Transform")?.transform.scale /= 2
+                immersiveContentEntity.findEntity(named: "Transform")?.transform.translation.z = 0.25
+                
+//                immersiveContentEntity.transform.translation = immersiveContentEntity.findEntity(named: "Transform")?.transform.translation ?? SIMD3()
+            }*/
+            
+            //Img tracking/detection
+//            await implementImgAnchor(content: content)
+            
+        }
+        attachments: {
             Attachment(id: "board") {
             }
         }
@@ -28,5 +97,82 @@ struct Arena: View {
                     .scaleEffect(3)
             }
         }
+        .gesture(
+            SpatialTapGesture()
+            .targetedToAnyEntity()
+            .onEnded {
+                $0.entity.applyTapForBehaviors()
+            }
+        )
     }
+        
+    
+    func implementImgAnchor(content: RealityViewContent) async {
+        
+        var entityMap : [UUID: ModelEntity] = [:]
+        var toPrint: Bool = true
+        
+        if true {
+            do {
+                try await arSession.run([imageInfo])
+            } catch {
+                print("error in img tracking provider")
+            }
+            
+            for await update in imageInfo.anchorUpdates {
+//                let _ = print(update.anchor.description)
+                let entity = ModelEntity(mesh: .generateSphere(radius: 2), materials: [simpleMaterial])
+                entityMap[update.anchor.id] = entity
+                rootEntity.addChild(entity)
+                entityMap[update.anchor.id]?.transform = Transform(matrix: update.anchor.originFromAnchorTransform)
+                content.add(entity)
+                if (toPrint){ print("Image anchor detected!"); toPrint = false }
+                
+            }
+        }
+    }
+                        
+    
+    //    var worldAnchor: Entity = AnchorEntity(.image(group: "CardDeck20", name: "IMG_4108"), trackingMode: .once)
+    
+    //    var body: some View {
+    //        RealityView { content in
+    //            content.add(commonModel.setupContentEntity())
+    
+    //            roadModel.root = commonModel.homeEntity
+    //            content.add(roadModel.myRoad)
+    //            commonModel.homeEntity.addChild(roadModel.yourRoad)
+    
+    
+    //            if let immersiveContentEntity = try? await Entity(named: "Immersive", in: realityKitContentBundle) {
+    //                content.add(immersiveContentEntity)
+    //            }
+    
+    //            await arkitViewModel.runSession()
+    
+    //            try? await arkitViewModel.runSession()
+    
+    
+    //            roadModel.root = worldAnchor
+    //            worldAnchor.addChild(roadModel.myRoad)
+    //            worldAnchor.addChild(roadModel.yourRoad)
+    //        }
+    //        .task {
+    //            do {
+    //                if commonModel.dataProvidersAreSupported && commonModel.isReadyToRun {
+    //                    try await commonModel.runSession()
+    //                } else {
+    //                    print("providers not supported or model not ready to run!!")
+    //                }
+    //            } catch {
+    //                print("Failed to start session!")
+    //            }
+    //        }
+    //        .task {
+    //            await commonModel.processUpdates()
+    //        }
+    //        .task(priority: .low) {
+    //            await commonModel.processLowUpdates()
+    //        }
+    //    }
 }
